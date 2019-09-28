fa95ee9cc1395e5e8aa95d4a290859683058c1e9
trevor992 <trevor.naumann92@gmail.com>
2019-09-28 16:33:35 -0700

    working on word count and training portion

--- NaiveBayesTextClassifier.py
+++ NaiveBayesTextClassifier.py
@@ -1,13 +1,32 @@
 import pandas as pd
+import numpy as np
+
+# opens file creates file objects
 train_ldoc = open('trainlabels.txt')
 train_ddoc = open('traindata.txt')
-train_dlist = train_ddoc.readlines()
-train_llist = train_ldoc.readlines()
-
-print(train_dlist)
-print(train_llist)
 
 
-'''  def word_count():
-    wordfreq = dict()
-'''
+# reads the file and splits the lines output is a list
+train_dlist = train_ddoc.read().splitlines()
+train_llist = train_ldoc.read().splitlines()
+# creates two dictionaries for storing word counts
+class1_dict = dict()
+class0_dict = dict()
+
+def word_count():
+    for i in train_dlist:
+        if train_llist[i] == 1:
+            word_list = train_dlist[i].split()
+            for w in word_list:
+                if word_list[w] not in class1_dict:
+
+
+        else:
+
+
+
+
+# print(train_dlist)
+# print(train_llist)
+
+
