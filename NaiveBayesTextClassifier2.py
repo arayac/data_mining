458ebb8d2cb26f73f9e8f10b0caddf7142efbac4
trevor992 <trevor.naumann92@gmail.com>
2019-09-30 13:41:59 -0700

    solved bug

--- NaiveBayesTextClassifier2.py
+++ NaiveBayesTextClassifier2.py
@@ -61,8 +61,11 @@ def create_vocab_dict(data):
 def calc_prior_prob(num_class0, num_class1):
     prior_probs = [0.0, 0.0]
     denom = num_class0 + num_class1
-    prior_probs[0] = np.log((float(num_class0) / float(denom)))
-    prior_probs[1] = np.log((float(num_class1) / float(denom)))
+    print(num_class0)
+    print(num_class1)
+    prior_probs[0] = (float(num_class0) / float(denom))
+    prior_probs[1] = (float(num_class1) / float(denom))
+    print(prior_probs)
     return prior_probs
 
 
@@ -84,13 +87,19 @@ def calc_cond_probs(class_dict0, class_dict1):
     countlist1 = class_dict1.values()
     totalwords0 = sum(countlist0)
     totalwords1 = sum(countlist1)
+    totalwords = totalwords1 + totalwords0
     vocab_sizes = vocab_size(class_dict0, class_dict1)
     for key in class_dict1:
-        class1_prob[key] = np.log((class_dict1[key] + 1) / (totalwords1 + vocab_sizes[2]))
+        class1_prob[key] = (class_dict1[key] + 1) / (totalwords1 + totalwords)
     for key in class_dict0:
-        class0_prob[key] = np.log((class_dict0[key] + 1) / (totalwords0 + vocab_sizes[2]))
+        class0_prob[key] = (class_dict0[key] + 1) / (totalwords0 + totalwords)
+    with open('outputf3.txt', 'w+') as outputf3:
+        outputf3.write(json.dumps(class1_prob))
+        outputf3.write('\n \n')
+        outputf3.write(json.dumps(class0_prob))
+        outputf3.write('\n \n')
 
-    return class0_prob, class1_prob, vocab_sizes[2]
+    return class0_prob, class1_prob, totalwords
 
 
 def train_mnb(data, labels):
@@ -98,10 +107,15 @@ def train_mnb(data, labels):
     doc_list1 = isolate_data(data, labels, '1')
     dict0, doc0count, listdict0 = create_vocab_dict(doc_list0)
     dict1, doc1count, listdict1 = create_vocab_dict(doc_list1)
+    with open('outputf2.txt', 'w+') as outputf2:
+        outputf2.write(json.dumps(dict0))
+        outputf2.write('\n \n')
+        outputf2.write(json.dumps(dict1))
+        outputf2.write('\n \n')
     vocab = create_vocab_list(dict0, dict1)
     priori_probs = calc_prior_prob(doc0count, doc1count)
-    class0_probs, class1_probs, vocabsize, = calc_cond_probs(dict0, dict1)
-    return class0_probs, class1_probs, priori_probs, vocab
+    class0_probs, class1_probs, totalwords = calc_cond_probs(dict0, dict1)
+    return class0_probs, class1_probs, priori_probs, vocab, totalwords
 
 
 # expects unk_data as a list of documents that need classifying
@@ -109,15 +123,17 @@ def classify_data(train_data, train_labels, unk_data):
     doc_res = ['0'] * len(unk_data)
     class0_calc = np.zeros(len(unk_data))
     class1_calc = np.zeros(len(unk_data))
-    class0_probs, class1_probs, priori_probs, vocab = train_mnb(train_data, train_labels)
+    class0_probs, class1_probs, priori_probs, vocab, totalwords = train_mnb(train_data, train_labels)
+    missing_word_prob = 1.0 / float(totalwords)
     unk_dict, unk_count, unk_list_dict = create_vocab_dict(unk_data)
+    print(unk_list_dict)
     for index,doc in enumerate(unk_list_dict):
-        class0_calc[index] += priori_probs[0]
-        class1_calc [index] += priori_probs[1]
+        class0_calc[index] += np.log(priori_probs[0])
+        class1_calc [index] += np.log(priori_probs[1])
         for w in doc:
             if w in vocab:
-                class0_calc[index] += class0_probs.get(w,0)
-                class1_calc[index] += class1_probs.get(w,0)
+                class0_calc[index] += np.log(class0_probs.get(w,missing_word_prob))
+                class1_calc[index] += np.log(class1_probs.get(w,missing_word_prob))
     for index, res in enumerate(doc_res):
         if class1_calc[index] >= class0_calc[index]:
             doc_res[index] = '1'
@@ -133,10 +149,8 @@ def calc_accuracy(results, corr_answers):
     return num_correct / num_tests
 
 
-
 class_res = classify_data(train_dlist, train_llist, train_dlist)
 accuracy = calc_accuracy(class_res, train_llist)
-
 
 
 with open('outputf.txt', 'w+') as outputf:
@@ -144,6 +158,7 @@ with open('outputf.txt', 'w+') as outputf:
     outputf.write('\n \n')
     outputf.write(str(accuracy))
     outputf.write('\n \n')
+
 
 
 
